ddd03fd87c5e56ad076f351fd443b74fa3e3b753
reza <rezafarhangoa@gmail.com>
2022-01-08 00:22:10 +0330

    import standard function of k-means to main body

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from functions import k_means, calculate_accuracy
+from functions import k_means, calculate_accuracy, standard_kmeans
+
 
 df = k_means(
     number_of_cluster=6,
@@ -10,3 +11,14 @@ df = k_means(
 
 accuracy = calculate_accuracy(df, 6)
 print(f"accuracy of clustering = {accuracy}%")
+
+dataset_dir = './Project2/dataset.csv'
+standard_kmeans(
+    dataset_dir=dataset_dir,
+    k=3,
+    ds_row=1,
+    ds_column=20,
+    x='absence possibility',
+    y='Month of absence'
+)
+
